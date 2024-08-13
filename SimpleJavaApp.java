6fe8d528cf4d0c76c770792311c101eaac761c9a
rakibinsky <rakibinsky@gmail.com>
2024-08-13 13:35:52 -0500

    pushing update

---
+++ SimpleJavaApp.java
@@ -0,0 +1,18 @@
+public class SimpleJavaApp {
+
+    public static void main(String[] args) {
+        // Create a variable to store the user's name
+        String userName = "Rakib";
+
+        // Print a greeting message to the console
+        System.out.println("Hello, " + userName + "! Welcome to your simple Java application.");
+
+        // Add a simple math operation
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        // Print the result of the math operation
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
