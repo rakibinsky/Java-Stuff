d093c7cd4be5b7a4c2b677f6d9b486dd5ec313fb
rakibinsky <rakibinsky@gmail.com>
2024-08-14 09:13:23 -0500

    new changes

---
+++ SystemInfo.java
@@ -0,0 +1,18 @@
+import java.util.Properties;
+
+public class SystemInfo {
+    public static void main(String[] args) {
+        // Get specific properties
+        String osName = System.getProperty("os.name");
+        String osVersion = System.getProperty("os.version");
+        String javaVersion = System.getProperty("java.version");
+
+        System.out.println("Operating System: " + osName);
+        System.out.println("OS Version: " + osVersion);
+        System.out.println("Java Version: " + javaVersion);
+
+        // Get all system properties
+        //Properties properties = System.getProperties();
+        //properties.list(System.out);
+    }
+}
